90a5c6b44c0c5ddc882f2c2316826ca672c483f5
Cristian Lemus <cristiandlemus1@gmail.com>
2024-02-29 00:02:28 -0500

    mandar id usuario en login

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ app.post('/login', async (req, res) => {
         // If username and password are correct, generate a JWT token
         const token = jwt.sign({ userId: userDoc.id }, 'lemus', { expiresIn: '1h' });
 
-        res.status(200).json({ "token": token, "user": userDoc, "success": true });
+        res.status(200).json({ "token": token, "user": userDoc.id, "success": true });
     } catch (error) {
         res.status(500).send({ "message": "Internal Server Error" });
     }
